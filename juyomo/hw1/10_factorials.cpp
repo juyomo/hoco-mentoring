29ebae44685c628076baf125ba592b261d4e5191
juyomo <j.92.moon@gmail.com>
2025-09-26 15:51:57 +0900

    Update 10_factorials.cpp

--- juyomo/hw1/10_factorials.cpp
+++ juyomo/hw1/10_factorials.cpp
@@ -4,13 +4,28 @@
 // HOCO Mentoring HW 1
 // https://github.com/juyomo/hoco-mentoring
 
+// Iterative, recursive 풀이 둘 다 해보았습니다! 참고해주세요 :)
+
 class Solution {
 public:
-    int factorial(int n) {
+    int factorialIterative(int n) {
+        int ans = 1;
+        for (int i = 2; i <= n; i++) {
+            ans *= i;
+        }
+        return ans;
+    }
+        
+    int factorialRecursive(int n) {
         if (n == 0) {
             return 1;
         } else {
             return n * factorial(n - 1);
         }
+    }
+    
+    int factorial(int n) {
+        // return factorialRecursive(n);
+        return factorialIterative(n);
     }
 };
