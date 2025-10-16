ad2111b4be4b1b60dcad241ab2052f66a9e13f18
juyomo <juyomo@users.noreply.github.com>
2025-10-16 15:51:55 +0900

    Add alternate solution

--- juyomo/hw3/41_bt_right_side_view.cpp
+++ juyomo/hw3/41_bt_right_side_view.cpp
@@ -53,3 +53,32 @@ public:
         return res;
     }
 };
+
+/* Alternate solution
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+
+        while (q.size() > 0) {
+            TreeNode* curr = q.front().first;
+            if (curr != nullptr) {
+                int depth = q.front().second;
+                int val = curr->val;
+              	
+                if (res.size() < depth + 1) {
+                    res.push_back(val);
+                }
+
+                q.push({curr->right, depth +1});
+                q.push({curr->left, depth +1});
+            }
+            q.pop();
+        }
+        return res;
+    }
+};
+*/
