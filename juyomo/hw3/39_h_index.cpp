fb664ca087273a1f913f3ba025bd1d16f0c1b444
juyomo <juyomo@users.noreply.github.com>
2025-10-18 16:54:58 +0900

    get rid of unnecessary code

--- juyomo/hw3/39_h_index.cpp
+++ juyomo/hw3/39_h_index.cpp
@@ -6,12 +6,8 @@
 
 class Solution {
 public:
-    static bool compare( int a, int b ) {
-        return a > b;
-    }
-
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end(), compare);
+        sort(citations.rbegin(), citations.rend());
         
         for (int i = citations.size() - 1; i >= 0; i--) {
             if (citations[i] >= i + 1) {
@@ -19,25 +15,5 @@ public:
             }
         }
         return 0;
-        
-        // want to find largest index i s.t.
-        // citations[i] >= i + 1
-        // return i + 1
-        int start = 0;
-        int end = citations.size() - 1;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (citations[mid] >= mid + 1 && citations[mid + 1] < mid + 1) {
-                return mid + 1;
-            }
-            
-            if (citations[mid] < mid + 1) {
-                end = mid - 1;
-            } else {
-                start = mid;
-            }
-        }
-        return start;
     }
 };
